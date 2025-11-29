8695e96ee21b31b8bffbcd1c0b04d811ac35af2d
Chris Bec <cdavidbm@gmail.com>
2025-11-29 10:29:44 -0500

    se añade Avante

--- init.lua
+++ init.lua
@@ -497,6 +497,39 @@ require("lazy").setup({
   end,
 },
 
+{
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false,
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = { insert_mode = true },
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = { file_types = { "markdown", "Avante" } },
+      ft = { "markdown", "Avante" },
+    },
+  },
+  opts = {
+    provider = "claude-code",
+  },
+},
+
 }, {
   -- OPCIONES DE LAZY.NVIM (segundo argumento)
   rocks = {
