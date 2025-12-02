d754370cdb499fb6389c25895810a60ee7a8b197
Chris Bec <cdavidbm@gmail.com>
2025-12-01 21:47:05 -0500

    linea doble en tmux y  nuevos keymaps para scnvim

--- lua/plugins/scnvim.lua
+++ lua/plugins/scnvim.lua
@@ -9,13 +9,18 @@ return {
     config = function()
       local scnvim = require("scnvim")
       local map = scnvim.map
+      local map_expr = scnvim.map_expr
 
       scnvim.setup({
         keymaps = {
           ["<C-e>"] = map("editor.send_block", { "n", "i" }),
-          ["<C-l>"] = map("editor.send_line", { "n", "i" }),
-          ["<C-k>"] = map("postwin.toggle", "n"),
-          ["<C-.>"] = map("sclang.hard_stop", { "n", "i" }),
+          ["<C-o>"] = map("editor.send_line", { "n", "i" }),
+          ["<C-p>"] = map("postwin.toggle", "n"),
+          ["<C-r>"] = map("sclang.hard_stop", { "n", "i" }),
+          ['<leader>sc'] = map('sclang.start'),
+          ['<F4>'] = map('sclang.stop'),
+          ['<F3>'] = map_expr('s.boot'),
+          ['<F2>'] = map_expr('s.meter'),
         },
         postwin = {
           float = {
